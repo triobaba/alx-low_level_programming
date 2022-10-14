5be3359e375fe24ea21b63e14b3b2115add26835
triobaba <akinolababalola20@mail.com>
2022-10-13 19:18:48 -0700

    a C programs that prints programming is like building a multilingual  puzzleuzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/** 
+ *main:This is the where the compiler runs the code from
+ *
+ *Return:Gives feedback to the std output
+ */
+int main(void)
+{	
+	puts( "Programming is like building a multilingual puzzle");
+	return(0);
+}
