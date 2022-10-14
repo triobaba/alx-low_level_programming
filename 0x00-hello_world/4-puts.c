d8e7edeb4bd6c82da793deaca4cdf414e9c0038d
triobaba <akinolababalola20@mail.com>
2022-10-13 19:32:13 -0700

    a C programs that prints programming is like building a multilingual  puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /** 
  *main:This is the where the compiler runs the code from
- *
+ *it begin compiling from the nmain function downwards
  *Return:Gives feedback to the std output
  */
 int main(void)
 {	
-	puts( "Programming is like building a multilingual puzzle");
-	return(0);
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
