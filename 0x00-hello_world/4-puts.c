6bf0e6d237dfd872c319fdae2603cb31c7a1eeef
triobaba <akinolababalola20@mail.com>
2022-10-13 19:38:25 -0700

    using puts instead of print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/** 
+/**
  *main:This is the where the compiler runs the code from
- *it begin compiling from the nmain function downwards
+ *
  *Return:Gives feedback to the std output
  */
 int main(void)
-{	
-	puts("Programming is like building a multilingual puzzle\n");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
