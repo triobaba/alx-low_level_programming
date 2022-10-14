4fb5917f84f34c15a5c8eaf40ffd250068f4b8e6
triobaba <akinolababalola20@mail.com>
2022-10-13 19:58:11 -0700

    Printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this is the part of the program where compilation starts,the main f.u
+ *
+ * Return: by default zero except it in a case where an erroe was encountered.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
